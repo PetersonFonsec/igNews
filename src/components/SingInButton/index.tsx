d471acb3ada2261962f5faaae226872e2e49e6eb
Peterson <petersonsimiao1@gmail.com>
2021-06-03 17:21:04 -0300

    feat: integrado autenticacao pelo github

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,18 +1,27 @@
 import styles from "./styles.module.scss";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-import { useState } from "react";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export function SingInButton() {
-  const [isUserLogged, setUserLogged] = useState(true);
-  return isUserLogged ? (
-    <button type="button" className={styles.SingInButton}>
+  const [session] = useSession();
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.SingInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Diego Fernandes
+      {session?.user?.name.split(" ")[0] || ""}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.SingInButton}>
+    <button
+      type="button"
+      className={styles.SingInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
